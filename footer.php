b88737860514830f4148cbb71aa009dd8ab0f9c5
Justin Torrance <justin.torrance91@gmail.com>
2020-07-17 10:13:26 -0600

    Initial commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+  <?php 
+    dynamic_sidebar('footer-1');
+  ?> 
+</footer>
+
+<?php
+  wp_footer();
+?>
+
---
+++ search.php
@@ -0,0 +1,21 @@
+<?php
+  get_header();
+?>
+
+<article class="content px-3 py-5 p-md-5">
+
+  <?php
+    if( have_posts() ){
+      while( have_posts() ){
+        the_post();
+        //looks for file path, finds type
+        get_template_part( 'templates-parts/content', 'archive' );
+      }
+    }
+  ?>
+  
+</article>
+
+<?php
+get_footer();
+?>
